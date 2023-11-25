39e94553afcc3e05a27e2ffe6a26ddea4a26825f
Fernanda Danieli <ferdanieli.danieli@gmail.com>
2023-11-25 19:25:35 -0300

    guardar

--- MiApp/urls.py
+++ MiApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from MiApp.views import crear_carrera, show_html
+from MiApp.views import crear_carrera, show_html, mostrar_carreras
 
 urlpatterns = [
     path('crear/', crear_carrera),
     path('show/', show_html),
+    path('carreras/', mostrar_carreras),
 ]
--- MiApp/views.py
+++ MiApp/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from MiApp.models import Carrera
+
+
+def mostrar_carreras(request):
+    carreras = Carrera.objects.all()
+    contexto = {
+        'carreras': carreras,
+
+    }
+    return render(request, 'MiApp/carreras.html', contexto)
 
 def crear_carrera(request):
     carrera = Carrera(nombre= "Logistica", comision= 12023)
     carrera.save()
-    contexto = {"carrera": carrera}
 
-    return render(request, 'index.html', contexto)
+    return redirect("/app/carreras/")
 
 def show_html(request):
     carrera = Carrera.objects.first()
